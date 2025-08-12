b02a0d4504b47efc5af4b041942a6f27ec9b68dd
yazan-sul <yazan-sul@gmail.com>
2025-08-12 13:59:51 +0300

    add place holders to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,17 +9,17 @@ export function Form() {
        <div className="form-row">
           <div className="form-group">
             <label>Name:</label>
-            <input type="text" name="name" />
+            <input type="text" name="name" placeholder="Your name"/>
           </div>
           <div className="form-group">
             <label>Email:</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" placeholder="example@gmail.com"/>
           </div>
         </div>
 
         <div className="form-group full-width">
           <label>Message:</label>
-          <textarea name="message"></textarea>
+          <textarea name="message" placeholder="Write your message here"></textarea>
         </div>
 
         <button type="submit">Send</button>
